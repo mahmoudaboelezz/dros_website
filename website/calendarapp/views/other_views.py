2ff80bfdb3253852dcef788a7654e5f3221621fb
aboelezz <mahmoudaboelezz51@gmail.com>
2022-11-04 16:42:04 +0200

    test sendgrid

--- website/calendarapp/views/other_views.py
+++ website/calendarapp/views/other_views.py
@@ -222,8 +222,8 @@ def member_join(request, event_id):
     )
     
 
-    # import os
-    # os.system('python manage.py sitemessage_send_scheduled')
+    import os
+    os.system('python3 manage.py sitemessage_send_scheduled')
     EventMember.objects.create(event=event, user=request.user)
     Newsletter.objects.create(user=request.user, email=request.user.email, name=request.user.username)
     # schedule_email(message=f'تم تسجيلك في دورة {event.title} بنجاح', email=email,sender=User.objects.get(id=1))
